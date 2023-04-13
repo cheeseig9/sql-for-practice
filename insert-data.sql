7227fc3d7c49b9702cf6588f6bcaf1339a0d8c6d
C.Seng <74439890+cheeseig9@users.noreply.github.com>
2023-04-13 18:31:46 +0800

    added

---
+++ insert-data.sql
@@ -0,0 +1,6 @@
+INSERT INTO student(NAME, major) VALUES('Jack', 'Biology');
+INSERT INTO student(NAME, major) VALUES('Kate', 'Sociology');
+INSERT INTO student(name) VALUES('Claire');
+INSERT INTO student(NAME, major) VALUES('Jack', 'Biology');
+INSERT INTO student(NAME, major) VALUES('Mike', 'Computer Science');
+
